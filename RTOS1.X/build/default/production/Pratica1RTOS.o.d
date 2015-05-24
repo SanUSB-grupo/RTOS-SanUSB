8aef037368d7ab422f68c1c11c8deaed7f7e1993
SanUSB <sanusb_laese@yahoo.com.br>
2015-05-24 15:21:31 -0300

    Ini7
    IniX

---
+++ RTOS1.X/build/default/production/Pratica1RTOS.o.d
@@ -0,0 +1,24 @@
+build/default/production/Pratica1RTOS.o:  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/Pratica1RTOS.c  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/SanUSB48.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/system/osa_tasks.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/timers/osa_ttimer.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/osa.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/OSAcfg.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/port/osa_include.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/port/pic18/osa_pic18_mplabc.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/osa_oldnames.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/system/osa_system.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/events/osa_bsem.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/events/osa_csem.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/events/osa_flag.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/events/osa_msg.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/events/osa_queue.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/events/osa_smsg.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/events/osa_squeue.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/timers/osa_stimer.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/timers/osa_stimer_old.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/timers/osa_dtimer.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/timers/osa_qtimer.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/timers/osa_timer.h  \
+
---
+++ RTOS1.X/build/default/production/osa/osa.o.d
@@ -0,0 +1,35 @@
+build/default/production/osa/osa.o:  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/osa.c  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/system/osa_tasks.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/timers/osa_ttimer.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/port/pic18/osa_pic18_mplabc.c  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/system/osa_system.c  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/system/osa_tasks.c  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/osa.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/OSAcfg.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/port/osa_include.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/port/pic18/osa_pic18_mplabc.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/osa_oldnames.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/system/osa_system.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/events/osa_bsem.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/events/osa_csem.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/events/osa_flag.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/events/osa_msg.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/events/osa_queue.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/events/osa_smsg.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/events/osa_squeue.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/timers/osa_stimer.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/timers/osa_stimer_old.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/timers/osa_dtimer.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/timers/osa_qtimer.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/timers/osa_timer.h  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/port/osa_include.c  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/timers/osa_dtimer.c  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/timers/osa_qtimer.c  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/timers/osa_stimer.c  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/timers/osa_ttimer.c  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/events/osa_squeue.c  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/events/osa_queue.c  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/events/osa_csem.c  \
+	C:/Users/Sandro/Dropbox/Public/RTOSB/RTOS1.X/osa/kernel/events/osa_bsem.c  \
+
